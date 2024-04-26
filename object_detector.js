1cf0e521356a3c428073e3e156f0013acf01e320
Abdoulaye Diop <adiop@solutiondatagroup.com>
2024-04-26 23:47:23 +0200

    Update for doployment on railway

--- object_detector.js
+++ object_detector.js
@@ -32,7 +32,7 @@ function main() {
     const port = process.env.PORT || 3000;
 
     app.listen(port, "0.0.0.0", () => {
-        console.log(`Server is listening on port 8080`)
+        console.log(`Server is listening on port ${port}`)
     });
 }
 
